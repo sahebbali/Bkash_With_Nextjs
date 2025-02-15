adf36e47dcbe34bfe49d20b67caa1390444ee344
sahebbali <saheb1996jmr@gmail.com>
2025-02-15 17:59:51 +0600

    add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,55 @@
+"use client";
 import Image from "next/image";
+import LaptopImage from "@/public/laptop.jpg"
+import axios from "axios";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  const [paynetData, setPaynetData] = useState({
+    email: "",
+    name: "",
+    phone: "",
+  });
+  const handlePayment = async () => {
+    try {
+      const { data } = await axios.post("/api/make-payment", paynetData);
+      if (data.url) {
+        router.push(data.url);
+      }
+    } catch (error) {
+      console.log(error);
+      alert("Something went wrong");
+    }
+  }
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="w-screen h-screen flex p-10">
+      <div className="w-1/3 h-full flex justify-center items-center">
+        <div className="w-full h-2/3">
+          <Image src={LaptopImage} alt="Laptop" className="rounded-lg" />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <div className="w-2/3 h-full flex flex-col justify-center items-center gap-5">
+        <div className="w-2/3 h-auto flex flex-col gap-3">
+          <h1 className="text-2xl font-bold">
+            Late 2019 Apple MacBook Pro with 2.6GHz Intel Core i7 (16-Inch, 16GB RAM, 512GB Storage) - Silver (Renewed)
+          </h1>
+          <p className="text-sm">
+            This pre-owned or refurbished product has been professionally inspected and tested to work and look like new. How a product becomes part of Amazon Renewed, your destination for pre-owned, refurbished products: A customer buys a new product and returns it or trades it in for a newer or different model. That product is inspected and tested to work and look like new by Amazon-qualified suppliers. Then, the product is sold as an Amazon Renewed product on Amazon. If not satisfied with the purchase, renewed products are eligible for replacement or refund under the Amazon Renewed Guarantee.
+          </p>
+        </div>
+        <div className="w-full h-auto flex flex-col justify-center items-center gap-3">
+          <div className="w-80 h-auto flex flex-col justify-center items-center gap-3">
+            <input onChange={(d) => setPaynetData({ ...paynetData, name: d.target.value })} type="text" className="w-full h-10 rounded-lg border-2 border-gray-300 p-2" placeholder="Name" />
+            <input onChange={(d) => setPaynetData({ ...paynetData, email: d.target.value })} type="text" className="w-full h-10 rounded-lg border-2 border-gray-300 p-2" placeholder="Email" />
+            <input onChange={(d) => setPaynetData({ ...paynetData, phone: d.target.value })} type="text" className="w-full h-10 rounded-lg border-2 border-gray-300 p-2" placeholder="Phone Number" />
+            <button onClick={() => handlePayment()} className="w-full h-10 rounded-lg bg-blue-500 text-white font-bold">
+              Buy Now
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
